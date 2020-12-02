9d966227d51a17b0f14917d44e4873cc2d903c5e
Sky <skyjillain@gmail.com>
2020-12-01 21:23:40 -0700

    access data in response, use response data in THML

--- Exercise Files/04_03_GiphyParameters/begin/script.js
+++ Exercise Files/04_03_GiphyParameters/begin/script.js
@@ -1,11 +1,27 @@
 var request = new XMLHttpRequest();
 
-request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=YOUR_API_KEY');
+request.open(
+  "GET",
+  "https://api.giphy.com/v1/gifs/random?api_key=VzfywmJixNSChZWkFqnuM4oeiuglLLNi&tag=cats&rating=g"
+);
 
-request.onload = function() {
-	var response = request.response;
-	var parsedData = JSON.parse(response);
-	console.log(parsedData);
-}
+request.onload = function () {
+  var response = request.response;
+  var parsedData = JSON.parse(response);
+  console.log(parsedData);
+
+  // search for original Url under: data/images/original/url  in Google console
+  var originalUrl = parsedData.data.images.original.url;
+  console.log(originalUrl);
+
+  // Create a new HTML, add original Url to our page showing different images while we refresh the page
+  var gif = document.createElement("img");
+  gif.setAttribute("src", originalUrl);
+  document.body.appendChild(gif);
+};
 
 request.send();
+// We then opened link in google console
+// Under data: bitly_git_url:"   "
+// the link in "    " showing cats GIPHY :):)
+// Pay attention to line 3:......?......&....&....
